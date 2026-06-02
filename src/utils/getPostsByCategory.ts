c95b07cb00309d5d144f1659ce3a55991eacfab4
phaooo <phaooo678@gmail.com>
2026-06-03 01:42:57 +0900

    Serve static holographic card from main (remove Astro project)
    Delete the Astro project and its GitHub Actions deploy workflow so the
    repo serves index.html directly via Pages (Deploy from a branch / root).
    Add .nojekyll so files are served as-is.

--- src/utils/getPostsByCategory.ts
+++
@@ -1,7 +0,0 @@
-import {flattenDeep} from 'lodash-es'
-import {dealLabel} from './dealLabel';
-
-const getPostsByCategory = (posts, category: string) =>
-  posts.filter(post => flattenDeep(dealLabel(post.data.category)).includes(category))
-
-export default getPostsByCategory;
--- src/utils/getPostsByTag.ts
+++
@@ -1,6 +0,0 @@
-import {flattenDeep} from 'lodash-es'
-import {dealLabel} from './dealLabel';
-
-const getPostsByTag = (posts, tag: string) =>
-  posts.filter(post => flattenDeep(dealLabel(post.data.tags)).includes(tag))
-export default getPostsByTag;
